2490fb844a46185010b39b2a175f7b04d184c875
Peter Ajtai <peter.ajtai@gmail.com>
2012-04-15 16:58:06 -0700

    gitignore and project beginning

---
+++ src/org/pa5/MotionHandler.java
@@ -0,0 +1,13 @@
+package org.pa5;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class MotionHandler extends Handler
+{
+    @Override
+    public void handleMessage(Message msg)
+    {
+        super.handleMessage(msg);
+    }
+}
---
+++ src/org/pa5/MovementStrategy.java
@@ -0,0 +1,6 @@
+package org.pa5;
+
+public interface MovementStrategy
+{
+    public int newPosition(int oldPosition);
+}

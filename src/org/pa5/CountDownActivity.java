0b9ea1a2b0428db9ba12a74be08fb7fe337b93f9
Peter Ajtai <peter.ajtai@gmail.com>
2012-04-15 16:05:09 -0700

    gitignore

--- src/org/pa5/CountDownActivity.java
+++ src/org/pa5/CountDownActivity.java
@@ -3,10 +3,13 @@ package org.pa5;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CountDownActivity extends Activity {
+public class CountDownActivity extends Activity
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        // test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }

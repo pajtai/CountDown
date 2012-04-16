22a5d185196d8a31559f2bc19160fbeed0203c11
Peter Ajtai <peter.ajtai@gmail.com>
2012-04-16 10:19:07 -0700

    Updating comments.

--- src/org/pa5/CountDownActivity.java
+++ src/org/pa5/CountDownActivity.java
@@ -11,8 +11,7 @@ import android.widget.TextView;
 
 /**
  * This activity implements a graphic count down timer.
- * The timer uses a Handler to calculate and time the motion of the count down numbers.
- * The handler uses the MovementStrategy interface to make its movement calculations.
+ * The timer uses a Handler to calculate and time the motion of the count down numbers. 
  * Each number is a TextView. The parent is a RelativeView.
  * 
  */
